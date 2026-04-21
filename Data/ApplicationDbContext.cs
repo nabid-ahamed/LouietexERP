ad78abe722a30b7ab02a1e3bd6346afcbba8e23b
Nabid Ahamed  Noushad <nabidahamed18@gmail.com>
2026-04-21 13:03:33 +0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LouietexERP.Models;
+
+namespace LouietexERP.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Inventory> Inventories { get; set; }
+    }
+}
---
+++ Models/ClassInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LouietexERP.Models
+{
+    public class Inventory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Material Name")]
+        public string Name { get; set; } // e.g., Cotton Fabric
+
+        public string Category { get; set; } // Fabric, Accessories 
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Minimum Stock Level")]
+        public int MinStockLevel { get; set; } // For Low Stock Alerts 
+
+        public string Supplier { get; set; }
+        
+    }
+}
